8983c2977aed2ea601707e60148025cb7a1cdf01
chavezE <chaaveze@gmail.com>
2018-02-08 16:12:06 -0600

    Make file and headers added

--- Test_without_parrot/color2gray.cpp
+++ Test_without_parrot/color2gray.cpp
@@ -2,6 +2,9 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+/* Developed routines*/
+#include "rutinasVision.h"
+
 using namespace std;
 using namespace cv;
 
@@ -34,6 +37,8 @@ int main(int argc, char *argv[])
 		imshow("Flipped", flippedImage);
 		imshow("Grayed",grayImage);
 
+		
+		key = waitKey(3);
 		/* If 'x' is pressed, exit program */
 		if (waitKey(3) == 'x')
 			break;
@@ -41,36 +46,3 @@ int main(int argc, char *argv[])
 }
 
 
-/*
- * This method flips horizontally the sourceImage into destinationImage. Because it uses 
- * "Mat::at" method, its performance is low (redundant memory access searching for pixels).
- */
-void flipImageBasic(const Mat &sourceImage, Mat &destinationImage)
-{
-	if (destinationImage.empty())
-		destinationImage = Mat(sourceImage.rows, sourceImage.cols, sourceImage.type());
-
-	for (int y = 0; y < sourceImage.rows; ++y)
-		for (int x = 0; x < sourceImage.cols / 2; ++x)
-			for (int i = 0; i < sourceImage.channels(); ++i)
-			{
-				destinationImage.at<Vec3b>(y, x)[i] = sourceImage.at<Vec3b>(y, sourceImage.cols - 1 - x)[i];
-				destinationImage.at<Vec3b>(y, sourceImage.cols - 1 - x)[i] = sourceImage.at<Vec3b>(y, x)[i];
-			}
-}
-
-void color2gray(const Mat &sourceImage, Mat &grayImage)
-{
-	int grayValue = 0;
-	for (int y = 0; y < sourceImage.rows; ++y){
-		for (int x = 0; x < sourceImage.cols ; ++x){
-			grayValue = 0;
-			for (int i = 0; i < sourceImage.channels(); ++i)
-			{
-				grayValue += sourceImage.at<Vec3b>(y, x)[i] ;
-			}
-			grayImage.at<uint8_t>(y,x) = grayValue/3;
-		}
-		
-	}
-}
