7de722ad7f5ed707f8f4ba654b2acc10674522ef
chavezE <chaaveze@gmail.com>
2018-02-11 23:12:53 -0600

    drone setup modifications

--- src/main/heli.cpp
+++ src/main/heli.cpp
@@ -14,10 +14,45 @@
 #include <stdio.h>
 #include <iostream>
 
+
+/* Developed routines*/
+#include "rutinasVision.h"
+
 using namespace std;
 using namespace cv;
 
-bool stop = false;
+/* Declaring all the image matrices */
+Mat bgrImage = Mat(240, 320, CV_8UC3);
+Mat bgrHistogram;
+Mat bgrFilter;
+Mat hsvImage;
+Mat hsvHistogram;
+Mat hsvFilter;
+Mat yiqImage;
+Mat yiqHistogram;
+Mat yiqFilter;
+Mat grayImage;
+Mat binImage;
+
+/* Setting the pixel coordinates and values in the different color models */
+int Px=0, Py=0;
+int vB=0, vG=0, vR=0;
+int vH=0, vS=0, vV=0;
+int vY=0, vI=0, vQ=0;
+
+/* Setting the color mode range for the filter
+   It's divided by channel 0 min, channel 0 max, channel 1 min, channel 1 max, channel 2 min, channel 2 max */
+int rBGR[] = {0, 255, 0, 255, 0, 255};
+int rHSV[] = {0, 255, 0, 255, 0, 255};
+int rYIQ[] = {0, 255, 0, 255, 0, 255};
+
+/* Slider values for binarization */
+int sliderBinValue=0;
+int const SLIDER_MAX = 255;
+
+
+
+
 CRawImage *image;
 CHeli *heli;
 float pitch, roll, yaw, height;
@@ -30,33 +65,7 @@ bool navigatedWithJoystick, joypadTakeOff, joypadLand, joypadHover;
 string ultimo = "init";
 
 
-// variable to store frame information upon clicks
-int Px;
-int Py;
-int vR;
-int vG;
-int vB;
-
 Mat imagenClick;
-
-
-/*
- * This method flips horizontally the sourceImage into destinationImage. Because it uses 
- * "Mat::at" method, its performance is low (redundant memory access searching for pixels).
- */
-void flipImageBasic(const Mat &sourceImage, Mat &destinationImage)
-{
-    if (destinationImage.empty())
-        destinationImage = Mat(sourceImage.rows, sourceImage.cols, sourceImage.type());
-
-    for (int y = 0; y < sourceImage.rows; ++y)
-        for (int x = 0; x < sourceImage.cols / 2; ++x)
-            for (int i = 0; i < sourceImage.channels(); ++i)
-            {
-                destinationImage.at<Vec3b>(y, x)[i] = sourceImage.at<Vec3b>(y, sourceImage.cols - 1 - x)[i];
-                destinationImage.at<Vec3b>(y, sourceImage.cols - 1 - x)[i] = sourceImage.at<Vec3b>(y, x)[i];
-            }
-}
 
 // Convert CRawImage to Mat
 void rawToMat( Mat &destImage, CRawImage* sourceImage)
@@ -71,48 +80,44 @@ void rawToMat( Mat &destImage, CRawImage* sourceImage)
     }
 }
 
-// Click en pantalla
-// void mouseCoordinatesExampleCallback(int event, int x, int y, int flags, void* param)
-// {
-//     switch (event)
-//     {
-//         case CV_EVENT_LBUTTONDOWN:
-//             cout << "  Mouse X, Y: " << x << ", " << y ;
-//             cout << endl;
-//             /*  Draw a point */
-//             points.push_back(Point(x, y));
-//             break;
-//         case CV_EVENT_MOUSEMOVE:
-//             break;
-//         case CV_EVENT_LBUTTONUP:
-//             break;
-//     }
-// }
 
-//codigo del click en pantalla
-void mouseCoordinatesExampleCallback(int event, int x, int y, int flags, void* param)
+void mCoordinatesComponentVal(int event, int x, int y, int flags, void* param)
 {
-    uchar* destination;
     switch (event)
     {
         case CV_EVENT_LBUTTONDOWN:
             Px=x;
             Py=y;
-            destination = (uchar*) imagenClick.ptr<uchar>(Py);
-            vB=destination[Px * 3];
-            vG=destination[Px*3+1];
-            vR=destination[Px*3+2];
+            
+            vB = bgrImage.at<Vec3b>(y, x)[0];
+            vG = bgrImage.at<Vec3b>(y, x)[1];
+            vR = bgrImage.at<Vec3b>(y, x)[2];
+
+            vH = hsvImage.at<Vec3b>(y, x)[0];
+            vS = hsvImage.at<Vec3b>(y, x)[1];
+            vV = hsvImage.at<Vec3b>(y, x)[2];
+
+            vY = yiqImage.at<Vec3b>(y, x)[0];
+            vI = yiqImage.at<Vec3b>(y, x)[1];
+            vQ = yiqImage.at<Vec3b>(y, x)[2];
+
+            // Print Coordinates and Values
+            cout <<"Pos X: "<<Px<<" Pos Y: "<<Py<<endl;
+            cout <<"Valor RGB: ("<<vR<<","<<vG<<","<<vB<<")"<<endl;
+            cout <<"Valor HSV: ("<<vH<<","<<vS<<","<<vV<<")"<<endl;
+            cout <<"Valor YIQ: ("<<vY<<","<<vI<<","<<vQ<<")"<<endl<<endl;
+
             break;
         case CV_EVENT_MOUSEMOVE:
             break;
         case CV_EVENT_LBUTTONUP:
             break;
         case CV_EVENT_RBUTTONDOWN:
-        //flag=!flag;
             break;
-        
     }
+    
 }
+
 
 int main(int argc,char* argv[])
 {
@@ -128,10 +133,6 @@ int main(int argc,char* argv[])
 
     // Destination OpenCV Mat   
     Mat currentImage = Mat(240, 320, CV_8UC3);
-
-    // Create flipped image space
-    Mat flippedImage;
-
     // Show it  
     imshow("ParrotCam", currentImage);
 
@@ -144,61 +145,191 @@ int main(int argc,char* argv[])
         m_joystick = SDL_JoystickOpen(0);
     }
 
-    namedWindow("Click");
-    setMouseCallback("Click", mouseCoordinatesExampleCallback);
 
 
 
+        /* Show interface menu to user*/
+    Mat menu;
+    menu.create(600,600,CV_8UC3);
+    menu.setTo(Scalar(200,200,200));
+    putText(menu,"Menu", Point(250,55) , FONT_HERSHEY_SIMPLEX, 1.5, Scalar(0,0,0), 2,8,false );
+    putText(menu,"p. Cambiar modo continuo / modo congelado", Point(15,90) , FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0,0,0), 2,8,false );
+    putText(menu,"r. Imagen RGB", Point(15,130) , FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0,0,0), 2,8,false );
+    putText(menu,"h. Imagen HSV", Point(15,170) , FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0,0,0), 2,8,false );
+    putText(menu,"y. Imagen YIQ", Point(15,210) , FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0,0,0), 2,8,false );
+    putText(menu,"b. Imagen gris / binarizacion", Point(15,250) , FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0,0,0), 2,8,false );
+    putText(menu,"q. Terminar", Point(15,290) , FONT_HERSHEY_SIMPLEX, 0.7, Scalar(0,0,0), 2,8,false );
+    imshow("Menu", menu);
 
-
-
-    bool freezeFrame = false;
-
-    /*  INFINITE LOOP */
-    while (stop == false)
+    bool freezeImage = false;
+    bool bContinue = true;
+    bool bEnRGB = false;
+    bool bEnYIQ = false;
+    bool bEnHSV = false;
+    bool bEnBinarization = false;
+    while (bContinue)
     {
 
-        /* ----------------------------------------------------- */
-        /*  TEST IMAGE CODE */
-        /* ----------------------------------------------------- */
 
 
-        if (!freezeFrame)
-        {
+        if(!freezeImage){
             //image is captured
             heli->renewImage(image);
 
             // Copy to OpenCV Mat
-            rawToMat(currentImage, image);
-            
+            rawToMat(bgrImage, image);
+            imshow("ParrotCam", bgrImage);
+
+
+            /* Obtain a new frame from camera */
+            //camera.read(bgrImage);
+
+            /* Calling routines to convert color spaces*/
+            color2gray(bgrImage,grayImage);
+            color2yiq(bgrImage,yiqImage);
+            cvtColor(bgrImage,hsvImage,CV_BGR2HSV);
         }
 
-        // show current image
-        imshow("ParrotCam", currentImage);
-
-        // Call flipped image function
-        flipImageBasic(currentImage, flippedImage);
-
-        // Show it
-        imshow("FlippedCam", flippedImage);
-
-        imagenClick=currentImage;
-        imshow("Click", imagenClick);
 
         
+        /* Show images */
+        if (bEnRGB)
+        {
+            namedWindow("RGB");
+            setMouseCallback("RGB", mCoordinatesComponentVal);
+            imshow("RGB", bgrImage);
+
+            imageHistogram(bgrImage, bgrHistogram);
+            Mat overlay;
+            bgrHistogram.copyTo(overlay);
+            rectangle(overlay, Rect(2*vB,0,2,400), Scalar(255,0,0), -1);
+            rectangle(overlay, Rect(2*vB,420,2,50), Scalar(255,255,255), -1);
+            rectangle(overlay, Rect(2*vG,0,2,400), Scalar(0,255,0), -1);
+            rectangle(overlay, Rect(2*vG,490,2,50), Scalar(255,255,255), -1);
+            rectangle(overlay, Rect(2*vR,0,2,400), Scalar(0,0,255), -1);
+            rectangle(overlay, Rect(2*vR,560,2,50), Scalar(255,255,255), -1);
+            addWeighted(overlay, 0.7, bgrHistogram, 0.3, 0, bgrHistogram);
+            namedWindow("RGB Histogram", CV_WINDOW_AUTOSIZE);
+            imshow("RGB Histogram", bgrHistogram);
+
+            namedWindow("RGB Filter");
+            colorFilter(bgrImage, bgrFilter, rBGR);
+            createTrackbar( "B Min Value", "RGB Filter", &rBGR[0], SLIDER_MAX, NULL);
+            createTrackbar( "B Max Value", "RGB Filter", &rBGR[1], SLIDER_MAX, NULL);
+            createTrackbar( "G Min Value", "RGB Filter", &rBGR[2], SLIDER_MAX, NULL);
+            createTrackbar( "G Max Value", "RGB Filter", &rBGR[3], SLIDER_MAX, NULL);
+            createTrackbar( "R Min Value", "RGB Filter", &rBGR[4], SLIDER_MAX, NULL);
+            createTrackbar( "R Max Value", "RGB Filter", &rBGR[5], SLIDER_MAX, NULL);
+            imshow("RGB Filter", bgrFilter);
+        }
+
+        if (bEnHSV)
+        {
+            namedWindow("HSV");
+            setMouseCallback("HSV", mCoordinatesComponentVal);
+            imshow("HSV", hsvImage);
+
+            imageHistogram(hsvImage, hsvHistogram);
+            Mat overlay;
+            hsvHistogram.copyTo(overlay);
+            rectangle(overlay, Rect(2*vH,0,2,400), Scalar(255,0,0), -1);
+            rectangle(overlay, Rect(2*vH,420,2,50), Scalar(255,255,255), -1);
+            rectangle(overlay, Rect(2*vS,0,2,400), Scalar(0,255,0), -1);
+            rectangle(overlay, Rect(2*vS,490,2,50), Scalar(255,255,255), -1);
+            rectangle(overlay, Rect(2*vV,0,2,400), Scalar(0,0,255), -1);
+            rectangle(overlay, Rect(2*vV,560,2,50), Scalar(255,255,255), -1);
+            addWeighted(overlay, 0.7, hsvHistogram, 0.3, 0, hsvHistogram);
+            namedWindow("HSV Histogram", CV_WINDOW_AUTOSIZE);
+            imshow("HSV Histogram", hsvHistogram);
+
+            namedWindow("HSV Filter");
+            colorFilter(hsvImage, hsvFilter, rHSV);
+            createTrackbar( "H Min Value", "HSV Filter", &rHSV[0], SLIDER_MAX, NULL);
+            createTrackbar( "H Max Value", "HSV Filter", &rHSV[1], SLIDER_MAX, NULL);
+            createTrackbar( "S Min Value", "HSV Filter", &rHSV[2], SLIDER_MAX, NULL);
+            createTrackbar( "S Max Value", "HSV Filter", &rHSV[3], SLIDER_MAX, NULL);
+            createTrackbar( "V Min Value", "HSV Filter", &rHSV[4], SLIDER_MAX, NULL);
+            createTrackbar( "V Max Value", "HSV Filter", &rHSV[5], SLIDER_MAX, NULL);
+            imshow("HSV Filter", hsvFilter);
+        }
+
+        if (bEnYIQ)
+        {
+            namedWindow("YIQ");
+            setMouseCallback("YIQ", mCoordinatesComponentVal);
+            imshow("YIQ", yiqImage);
+
+            imageHistogram(yiqImage, yiqHistogram);
+            Mat overlay;
+            yiqHistogram.copyTo(overlay);
+            rectangle(overlay, Rect(2*vY,0,2,400), Scalar(255,0,0), -1);
+            rectangle(overlay, Rect(2*vY,420,2,50), Scalar(255,255,255), -1);
+            rectangle(overlay, Rect(2*vI,0,2,400), Scalar(0,255,0), -1);
+            rectangle(overlay, Rect(2*vI,490,2,50), Scalar(255,255,255), -1);
+            rectangle(overlay, Rect(2*vQ,0,2,400), Scalar(0,0,255), -1);
+            rectangle(overlay, Rect(2*vQ,560,2,50), Scalar(255,255,255), -1);
+            addWeighted(overlay, 0.7, yiqHistogram, 0.3, 0, yiqHistogram);
+            namedWindow("YIQ Histogram", CV_WINDOW_AUTOSIZE);
+            imshow("YIQ Histogram", yiqHistogram);
+
+            namedWindow("YIQ Filter");
+            colorFilter(yiqImage, yiqFilter, rYIQ);
+            createTrackbar( "Y Min Value", "YIQ Filter", &rYIQ[0], SLIDER_MAX, NULL);
+            createTrackbar( "Y Max Value", "YIQ Filter", &rYIQ[1], SLIDER_MAX, NULL);
+            createTrackbar( "I Min Value", "YIQ Filter", &rYIQ[2], SLIDER_MAX, NULL);
+            createTrackbar( "I Max Value", "YIQ Filter", &rYIQ[3], SLIDER_MAX, NULL);
+            createTrackbar( "Q Min Value", "YIQ Filter", &rYIQ[4], SLIDER_MAX, NULL);
+            createTrackbar( "Q Max Value", "YIQ Filter", &rYIQ[5], SLIDER_MAX, NULL);
+            imshow("YIQ Filter", yiqFilter);
+        }
+
+        if (bEnBinarization)
+        {
+            /* Create Trackbar */
+            gray2threshold(grayImage,binImage,sliderBinValue);
+            namedWindow("Gray Binarization",1);
+            createTrackbar( "Binatization threshold", "Gray Binarization", &sliderBinValue, SLIDER_MAX, NULL);
+            imshow("Gray", grayImage);
+            imshow("Gray Binarization", binImage);
+        }
 
 
 
+        uint8_t key = waitKey(3);
+        switch (key)
+        {
+            /* Exit -> 'ESC' key or q key*/
+            case 'q':
+            case 27:
+                cout << "Terminating program" << endl;
+                bContinue = false;
+                break;
+            case 'p':
+                freezeImage = !freezeImage;
+                break;
+            case 'r':
+                if(bEnRGB){ destroyWindow("RGB"); destroyWindow("RGB Histogram"); destroyWindow("RGB Filter"); }
+                bEnRGB = !bEnRGB;
+                break;
+            case 'h':
+                if(bEnHSV){ destroyWindow("HSV"); destroyWindow("HSV Histogram"); destroyWindow("HSV Filter"); }
+                bEnHSV = !bEnHSV;
+                break;
+            case 'y':
+                if(bEnYIQ){ destroyWindow("YIQ"); destroyWindow("YIQ Histogram"); destroyWindow("YIQ Filter"); }
+                bEnYIQ = !bEnYIQ;
+                break;
+            case 'b':
+                if(bEnBinarization){ destroyWindow("Gray"); destroyWindow("Gray Binarization"); }
+                bEnBinarization = !bEnBinarization;
+                break;
 
-        /*  Click events */
-        char key = waitKey(5);
-        switch (key) {
-            /* Control Parrot */
+            // Parrot commands
             case 'a': yaw = -20000.0; break;
             case 'd': yaw = 20000.0; break;
             case 'w': height = -20000.0; break;
             case 's': height = 20000.0; break;
-            case 'q': heli->takeoff(); break;
+            case 't': heli->takeoff(); break;
             case 'e': heli->land(); break;
             case 'z': heli->switchCamera(0); break;
             case 'x': heli->switchCamera(1); break;
@@ -208,32 +339,10 @@ int main(int argc,char* argv[])
             case 'l': roll = 20000.0; break;
             case 'i': pitch = -20000.0; break;
             case 'k': pitch = 20000.0; break;
-            case 'h': hover = (hover + 1) % 2; break;
-            case 27: stop = true; break;
-
-
-            /* Tests */
-
-            // freeze frame
-            case 'p':
-                freezeFrame = !freezeFrame;
-
-
-            /* Default */
-            default: pitch = roll = yaw = height = 0.0;
-
-
+            case 'o': hover = (hover + 1) % 2; break;
         }
 
 
-
-
-        /* ----------------------------------------------------- */
-        /*  PARRTO CODE */
-        /* ----------------------------------------------------- */
-
-        // Clear the console
-        printf("\033[2J\033[1;1H");
 
         if (useJoystick)
         {
@@ -250,25 +359,21 @@ int main(int argc,char* argv[])
         }
 
         // prints the drone telemetric data, helidata struct contains drone angles, speeds and battery status
-        printf("===================== Parrot Basic Example =====================\n\n");
-        fprintf(stdout, "Angles  : %.2lf %.2lf %.2lf \n", helidata.phi, helidata.psi, helidata.theta);
-        fprintf(stdout, "Speeds  : %.2lf %.2lf %.2lf \n", helidata.vx, helidata.vy, helidata.vz);
-        fprintf(stdout, "Battery : %.0lf \n", helidata.battery);
-        fprintf(stdout, "Hover   : %d \n", hover);
-        fprintf(stdout, "Joypad  : %d \n", useJoystick ? 1 : 0);
-        fprintf(stdout, "  Roll    : %d \n", joypadRoll);
-        fprintf(stdout, "  Pitch   : %d \n", joypadPitch);
-        fprintf(stdout, "  Yaw     : %d \n", joypadYaw);
-        fprintf(stdout, "  V.S.    : %d \n", joypadVerticalSpeed);
-        fprintf(stdout, "  TakeOff : %d \n", joypadTakeOff);
-        fprintf(stdout, "  Land    : %d \n", joypadLand);
-        fprintf(stdout, "Navigating with Joystick: %d \n", navigatedWithJoystick ? 1 : 0);
+        // printf("===================== Parrot Basic Example =====================\n\n");
+        // fprintf(stdout, "Angles  : %.2lf %.2lf %.2lf \n", helidata.phi, helidata.psi, helidata.theta);
+        // fprintf(stdout, "Speeds  : %.2lf %.2lf %.2lf \n", helidata.vx, helidata.vy, helidata.vz);
+        // fprintf(stdout, "Battery : %.0lf \n", helidata.battery);
+        // fprintf(stdout, "Hover   : %d \n", hover);
+        // fprintf(stdout, "Joypad  : %d \n", useJoystick ? 1 : 0);
+        // fprintf(stdout, "  Roll    : %d \n", joypadRoll);
+        // fprintf(stdout, "  Pitch   : %d \n", joypadPitch);
+        // fprintf(stdout, "  Yaw     : %d \n", joypadYaw);
+        // fprintf(stdout, "  V.S.    : %d \n", joypadVerticalSpeed);
+        // fprintf(stdout, "  TakeOff : %d \n", joypadTakeOff);
+        // fprintf(stdout, "  Land    : %d \n", joypadLand);
+        // fprintf(stdout, "Navigating with Joystick: %d \n", navigatedWithJoystick ? 1 : 0);
+        // cout<<"Pos X: "<<Px<<" Pos Y: "<<Py<<" Valor RGB: ("<<vR<<","<<vG<<","<<vB<<")"<<endl;
         
-
-        cout<<"Pos X: "<<Px<<" Pos Y: "<<Py<<" Valor RGB: ("<<vR<<","<<vG<<","<<vB<<")"<<endl;
-        
-        
-
 
         if (joypadTakeOff) {
             heli->takeoff();
---
+++ src/main/rutinasVision.h
@@ -0,0 +1,151 @@
+#ifndef rutinasVision_h
+#define rutinasVision_h
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+void color2gray(const Mat &sourceImage, Mat &destinationImage)
+{
+	if (destinationImage.empty())
+		destinationImage = Mat(sourceImage.rows, sourceImage.cols, CV_8UC1);
+
+	int grayValue = 0;
+	for (int y = 0; y < sourceImage.rows; ++y){
+		for (int x = 0; x < sourceImage.cols ; ++x){
+			grayValue = 0;
+			for (int i = 0; i < sourceImage.channels(); ++i)
+			{
+				grayValue += sourceImage.at<Vec3b>(y, x)[i] ;
+			}
+			destinationImage.at<uint8_t>(y,x) = grayValue/3;
+		}
+	}
+}
+
+void color2yiq(const Mat &sourceImage, Mat &destinationImage)
+{
+	if (destinationImage.empty())
+		destinationImage = Mat(sourceImage.rows, sourceImage.cols, sourceImage.type());
+
+	for (int y = 0; y < sourceImage.rows; ++y){
+		for (int x = 0; x < sourceImage.cols; ++x){
+			int vB = sourceImage.at<Vec3b>(y, x)[0];
+            int vG = sourceImage.at<Vec3b>(y, x)[1];
+            int vR = sourceImage.at<Vec3b>(y, x)[2];
+
+			destinationImage.at<Vec3b>(y, x)[0] = int(0.299*vR + 0.587*vG + 0.114*vB);
+			destinationImage.at<Vec3b>(y, x)[1] = int(0.596*vR - 0.275*vG - 0.321*vB);
+			destinationImage.at<Vec3b>(y, x)[2] = int(0.212*vR - 0.523*vG + 0.311*vB);
+		}
+	}
+}
+
+void colorFilter(const Mat &sourceImage, Mat &destinationImage, int range[6])
+{
+	if (destinationImage.empty())
+		destinationImage = Mat(sourceImage.rows, sourceImage.cols, sourceImage.type());
+
+	for (int y = 0; y < sourceImage.rows; ++y){
+		for (int x = 0; x < sourceImage.cols; ++x){
+			int v0 = sourceImage.at<Vec3b>(y, x)[0];
+            int v1 = sourceImage.at<Vec3b>(y, x)[1];
+            int v2 = sourceImage.at<Vec3b>(y, x)[2];
+
+            if(v0 >= range[0] && v0 <= range[1] && v1 >= range[2] && v1 <= range[3] && v2 >= range[4] && v2 <= range[5])
+            	destinationImage.at<Vec3b>(y, x) = sourceImage.at<Vec3b>(y, x);
+            else
+            	destinationImage.at<Vec3b>(y, x) = Vec3b(0,0,0);
+		}
+	}
+}
+
+void gray2threshold(const Mat &sourceImage, Mat &binImage, uint8_t threshold_value){
+  /* 
+  	Last param is type
+  	0: Binary
+    1: Binary Inverted
+    2: Threshold Truncated
+    3: Threshold to Zero
+    4: Threshold to Zero Inverted
+   */
+
+  threshold( sourceImage, binImage, threshold_value, 255,0 );
+}
+
+void imageHistogram(const Mat &sourceImage, Mat &destinationImage)
+{
+  /// Separate the image in 3 places ( B, G and R )
+  vector<Mat> bgr_planes;
+  split( sourceImage, bgr_planes );
+
+  /// Establish the number of bins
+  int histSize = 256;
+
+  /// Set the ranges ( for B,G,R) )
+  float range[] = { 0, 256 } ;
+  const float* histRange = { range };
+
+  bool uniform = true; bool accumulate = false;
+
+  Mat b_hist, g_hist, r_hist;
+
+  /// Compute the histograms:
+  calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
+  calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
+  calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
+
+  // Draw the histograms for B, G and R
+  int hist_w = 512; int hist_h = 400;
+  int bin_w = cvRound( (double) hist_w/histSize );
+
+  Mat histImage( hist_h + 210, hist_w, CV_8UC3, Scalar( 0,0,0) );
+
+  /// Normalize the result to [ 0, histImage.rows ]
+  normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+  normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+  normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+
+  /// Draw for each channel
+  for( int i = 1; i < histSize; i++ )
+  {
+      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
+                       Scalar( 255, 0, 0), 2, 8, 0  );
+      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
+                       Scalar( 0, 255, 0), 2, 8, 0  );
+      line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
+                       Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
+                       Scalar( 0, 0, 255), 2, 8, 0  );
+  }
+
+  /// Gray line divisor
+  for(int y = hist_h; y < hist_h + 20; y++)
+  {
+  	  for(int x = 0; x < hist_w; x++)
+  	  {
+	  	  histImage.at<Vec3b>(y, x) = Vec3b(200,200,200);
+	  	  histImage.at<Vec3b>(y + 70, x) = Vec3b(200,200,200);
+	  	  histImage.at<Vec3b>(y + 140, x) = Vec3b(200,200,200);
+  	  }
+  }
+
+  /// Channel gradients
+  for(int y = hist_h + 20; y < hist_h + 70; y++)
+  {
+  	  for(int x = 0; x < hist_w; x++)
+  	  {
+	  	  histImage.at<Vec3b>(y, x) = Vec3b(x/2,0,0);
+	  	  histImage.at<Vec3b>(y + 70, x) = Vec3b(0,x/2,0);
+	  	  histImage.at<Vec3b>(y + 140, x) = Vec3b(0,0,x/2);
+  	  }
+  }
+
+  destinationImage = histImage;
+}
+
+#endif
